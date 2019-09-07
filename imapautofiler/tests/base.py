11ada34e4e9802f4a9522926ab8eccf2d1b6e6ca
Doug Hellmann <doug@doughellmann.com>
2019-09-07 10:47:58 -0400

    remove import to fix pep8 error
    A later import was redefining the name of the datetime module because
    it imports the class directly.
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- imapautofiler/tests/base.py
+++ imapautofiler/tests/base.py
@@ -10,7 +10,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import datetime
 import email.parser
 from email.header import Header
 from email.message import Message
